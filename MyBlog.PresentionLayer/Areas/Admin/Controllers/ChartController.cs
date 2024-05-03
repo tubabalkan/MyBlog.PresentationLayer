6e4dbc48955771e0313ad4547ba3afefaa4731da
tuba balkan <tubabalkan165@gmail.com>
2024-05-03 14:25:36 +0300

    admin area eklendi

---
+++ MyBlog.PresentionLayer/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.PresentationLayer.Areas.Admin.Controllers
+{
+    public class ChartController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult CharttList()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyBlog.PresentionLayer/Areas/Admin/Controllers/StatistikController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.PresentationLayer.Areas.Admin.Controllers
+{
+    public class StatistikController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult StatistikList()
+        {
+            return View();
+        }
+    }
+}
