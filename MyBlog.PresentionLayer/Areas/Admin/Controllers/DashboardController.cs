1c09c357ae6e811c1103334c19bbee878418c0be
tuba balkan <tubabalkan165@gmail.com>
2024-05-08 21:20:08 +0300

    admin kısmında yazarlar tablosu ve dashboard eklendi

--- MyBlog.PresentionLayer/Areas/Admin/Controllers/DashboardController.cs
+++ MyBlog.PresentionLayer/Areas/Admin/Controllers/DashboardController.cs
@@ -1,13 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.BusinessLayer.Abstract;
 
 namespace MyBlog.PresentationLayer.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Route("/Admin/Dashboard")]
     public class DashboardController : Controller
     {
+        private readonly ICategoryService _categoryService;
+        private readonly ICommentService _commentService;
+        private readonly ITagService _tagService;
+        private readonly IWriterService _writerService;
+        private readonly IArticleService _articleService;
+
+        public DashboardController(ICategoryService categoryService, ICommentService commentService, ITagService tagService, IWriterService writerService, IArticleService articleService)
+        {
+            _categoryService = categoryService;
+            _commentService = commentService;
+            _tagService = tagService;
+            _writerService = writerService;
+            _articleService = articleService;
+        }
+        [HttpGet]
+        [Route("DashboardList")]
         public IActionResult DashboardList()
         {
+            ViewBag.category = _categoryService.TGetListAll().Count;
+            ViewBag.comment = _commentService.TGetListAll().Count;
+            ViewBag.Tag = _tagService.TGetListAll().Count;
+            ViewBag.writer = _writerService.TGetListAll().Count;
+
+
+          
+
+
+
+            
+            
             return View();
         }
+       
     }
 }
---
+++ MyBlog.PresentionLayer/Areas/Admin/Controllers/YazarlarController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.BusinessLayer.Abstract;
+using MyBlog.EntityLayer.Concrete;
+
+namespace MyBlog.PresentationLayer.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class YazarlarController : Controller
+    {
+        private readonly IWriterService _writerService;
+
+        public YazarlarController(IWriterService writerService)
+        {
+            _writerService = writerService;
+        }
+      
+        public IActionResult yazarlarListesi()
+        {
+
+            var values = _writerService.TGetListAll();
+            ViewBag.writer = _writerService.TGetListAll().Count;
+            return View(values);
+        }
+
+        public IActionResult YazarSil(int id)
+        {
+            _writerService.TDelete(id);
+            return RedirectToAction("yazarlarListesi");
+        }
+       
+        [HttpGet]
+
+        public IActionResult YazarGuncelle(int id)
+        {
+            var values = _writerService.TGetById(id);
+            return View(values);
+        }
+        [HttpPost]
+
+        public IActionResult YazarGuncelle(Writer writer)
+        {
+            _writerService.TUpdate(writer);
+            return RedirectToAction("yazarlarListesi");
+        }
+        [HttpGet]
+
+        public IActionResult YeniYazar()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult YeniYazar(Writer writer)
+        {
+            _writerService.TInsert(writer);
+            return RedirectToAction("yazarlarListesi");
+        }
+    }
+}
