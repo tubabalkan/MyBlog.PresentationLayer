44e71e136f660f144cca40f4b763adfbf869ddc3
tuba balkan <tubabalkan165@gmail.com>
2024-05-08 18:41:20 +0300

    dashboard sayfası tasarımı yapıldı

---
+++ MyBlog.PresentionLayer/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.PresentationLayer.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult DashboardList()
+        {
+            return View();
+        }
+    }
+}
