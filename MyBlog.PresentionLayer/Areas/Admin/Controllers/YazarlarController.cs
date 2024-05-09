73efb6a63c3dc27b5a9d2fae69ada323688c32f2
tuba balkan <tubabalkan165@gmail.com>
2024-05-09 20:46:42 +0300

    eklemeler yapıldı

--- MyBlog.PresentionLayer/Areas/Admin/Controllers/YazarlarController.cs
+++ MyBlog.PresentionLayer/Areas/Admin/Controllers/YazarlarController.cs
@@ -29,27 +29,23 @@ namespace MyBlog.PresentationLayer.Areas.Admin.Controllers
         }
        
         [HttpGet]
-
         public IActionResult YazarGuncelle(int id)
         {
             var values = _writerService.TGetById(id);
             return View(values);
         }
         [HttpPost]
-
         public IActionResult YazarGuncelle(Writer writer)
         {
             _writerService.TUpdate(writer);
             return RedirectToAction("yazarlarListesi");
         }
         [HttpGet]
-
         public IActionResult YeniYazar()
         {
             return View();
         }
         [HttpPost]
-
         public IActionResult YeniYazar(Writer writer)
         {
             _writerService.TInsert(writer);
