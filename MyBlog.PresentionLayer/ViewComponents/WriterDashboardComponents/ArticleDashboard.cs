298941072b92cfee3fc13f7c871de22a5cc13165
tuba balkan <tubabalkan165@gmail.com>
2024-05-09 15:39:26 +0300

    yazar areada dashboard işlemleri yapıldı

---
+++ MyBlog.PresentionLayer/ViewComponents/WriterDashboardComponents/ArticleDashboard.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.BusinessLayer.Abstract;
+
+namespace MyBlog.PresentationLayer.ViewComponents.WriterDashboardComponents
+{
+    public class ArticleDashboard : ViewComponent
+    {
+        private readonly IArticleService _articleService;
+
+        public ArticleDashboard(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _articleService.TGetListAll().Take(3).ToList();
+            return View(values);
+        }
+    }
+}
---
+++ MyBlog.PresentionLayer/ViewComponents/WriterDashboardComponents/MessageListDashboard.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyBlog.BusinessLayer.Abstract;
+
+namespace MyBlog.PresentationLayer.ViewComponents.WriterDashboardComponents
+{
+    public class MessageListDashboard : ViewComponent
+    {
+        private readonly IMessageService _messageService;
+
+        public MessageListDashboard(IMessageService messageService)
+        {
+            _messageService = messageService;
+        }
+        public IViewComponentResult Invoke()
+        {
+           
+            var values = _messageService.TGetListAll().Take(7).ToList();
+            return View(values);
+        }
+    }
+}
